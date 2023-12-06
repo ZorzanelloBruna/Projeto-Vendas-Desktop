8256519b50ad9b148995c3276f3fac8c1bce71cf
Bruna Zorzanello <brunazorzanello@gmail.com>
2023-12-06 15:11:46 -0300

    criando método de listar clientes

--- src/projetovendas/dao/ClientesDAO.java
+++ src/projetovendas/dao/ClientesDAO.java
@@ -6,7 +6,10 @@ package projetovendas.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 import projetovendas.jdbc.ConnectionFactory;
 import projetovendas.model.Clientes;
@@ -64,5 +67,45 @@ public class ClientesDAO {
      public void excluirCilente(){
         
     }
+     
+     public List<Clientes> listarCliente(){
+         
+         try {
+             
+             //1 passo - criar a lista             
+             List<Clientes> lista = new ArrayList<>();
+             
+             //2 passo -criar o comando sql,organizar e executar
+             String sql = "select * from tb_clientes";
+             PreparedStatement stmt = con.prepareStatement(sql);             
+             ResultSet rs = stmt.executeQuery();
+             
+             while(rs.next()){
+                 Clientes obj = new Clientes();
+                 obj.setId(rs.getInt("id"));
+                 obj.setNome(rs.getString("nome"));
+                 obj.setRg(rs.getString("rg"));
+                 obj.setCpf(rs.getString("cpf"));
+                 obj.setEmail(rs.getString("email"));
+                 obj.setTelefone(rs.getString("telefone"));
+                 obj.setCelular(rs.getString("celular"));
+                 obj.setCep(rs.getString("cep"));
+                 obj.setEndereco(rs.getString("endereco"));
+                 obj.setNumero(rs.getInt("numero"));
+                 obj.setComplemento(rs.getString("complemento"));
+                 obj.setBairro(rs.getString("bairro"));
+                 obj.setCidade(rs.getString("cidade"));
+                 obj.setEstado(rs.getString("estado"));
+                 
+                 lista.add(obj);
+             }                          
+             return  lista;
+             
+         } catch (SQLException e) {
+             JOptionPane.showMessageDialog(null, "Erro ao listar os dados!" + e);
+             return null;
+         }
+         
+     }
     
 }
