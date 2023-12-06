0db94d7a3cb2becf132ff6c33b873a9fe773eb15
Bruna Zorzanello <brunazorzanello@gmail.com>
2023-12-06 16:08:29 -0300

    criação do método alterar

--- src/projetovendas/dao/ClientesDAO.java
+++ src/projetovendas/dao/ClientesDAO.java
@@ -60,8 +60,38 @@ public class ClientesDAO {
         
     }
     
-      public void alterarCilente(){
-        
+      public void alterarCilente(Clientes obj){
+        try {
+            //1° passo - criar comamndo sql
+            String sql = " update tb_clientes set nome=?, rg=?, cpf=?,email=?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,"
+                    + "bairro=?,cidade=?,estado=?  where id=?";
+            
+            //2° passo - conectar o banco de dados e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getEstado());
+            stmt.setInt(14, obj.getId());
+            
+            //3° passo - executar o comando sql
+            stmt.execute();
+            stmt.close();
+            
+            JOptionPane.showMessageDialog(null,"Alteração realizada com Sucesso!!");
+            
+        } catch (SQLException e) {
+              JOptionPane.showMessageDialog(null,"Erro ao realizar alteração." + e);
+        }
     }
     
      public void excluirCilente(Clientes obj){
