24b0563d1da7d79ed2691bd9e38e1ba9001b627d
Bruna Zorzanello <brunazorzanello@gmail.com>
2023-12-18 14:37:34 -0300

    criação do método alterar dados do fncionario

--- src/projetovendas/dao/FuncionariosDAO.java
+++ src/projetovendas/dao/FuncionariosDAO.java
@@ -65,11 +65,12 @@ public class FuncionariosDAO {
         }
 
     }
-    
+        
+        //Método alterar dados do Funcionário
       public void alterarFuncionario(Funcionarios obj) {
         try {
             //1° passo - criar comamndo sql
-            String sql = " update tb_clientes set nome=?, rg=?, cpf=?,email=?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,"
+            String sql = " update tb_funcionarios set nome=?, rg=?, cpf=?,email=?,senha=?,cargo=?,nivel_acesso=?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,"
                     + "bairro=?,cidade=?,estado=?  where id=?";
 
             //2° passo - conectar o banco de dados e organizar o comando sql
@@ -78,16 +79,19 @@ public class FuncionariosDAO {
             stmt.setString(2, obj.getRg());
             stmt.setString(3, obj.getCpf());
             stmt.setString(4, obj.getEmail());
-            stmt.setString(5, obj.getTelefone());
-            stmt.setString(6, obj.getCelular());
-            stmt.setString(7, obj.getCep());
-            stmt.setString(8, obj.getEndereco());
-            stmt.setInt(9, obj.getNumero());
-            stmt.setString(10, obj.getComplemento());
-            stmt.setString(11, obj.getBairro());
-            stmt.setString(12, obj.getCidade());
-            stmt.setString(13, obj.getEstado());
-            stmt.setInt(14, obj.getId());
+            stmt.setString(5, obj.getSenha());
+            stmt.setString(6, obj.getCargo());
+            stmt.setString(7, obj.getNivel_acesso());
+            stmt.setString(8, obj.getTelefone());
+            stmt.setString(9, obj.getCelular());
+            stmt.setString(10, obj.getCep());
+            stmt.setString(11, obj.getEndereco());
+            stmt.setInt(12, obj.getNumero());
+            stmt.setString(13, obj.getComplemento());
+            stmt.setString(14, obj.getBairro());
+            stmt.setString(15, obj.getCidade());
+            stmt.setString(16, obj.getEstado());
+            stmt.setInt(17, obj.getId());
 
             //3° passo - executar o comando sql
             stmt.execute();
