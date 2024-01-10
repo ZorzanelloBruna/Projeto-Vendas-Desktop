b0669c2489de576ec6810e3354cc08eb43792906
Bruna Zorzanello <brunazorzanello@gmail.com>
2024-01-10 15:25:02 -0300

    criação do método cadastrarVenda

---
+++ src/projetovendas/dao/VendasDAO.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetovendas.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import projetovendas.jdbc.ConnectionFactory;
+import projetovendas.model.Vendas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class VendasDAO {
+       private Connection con;
+
+    public VendasDAO() {
+        this.con = new ConnectionFactory().getConnection();
+    }
+    //cadastrar Venda
+    public void cadastrarVenda(Vendas obj){
+          try {
+            String sql = "insert into tb_vendas(cliente_id, data_venda, total_venda, observacoes) values (?,?,?,?)";
+            //organizando as informações para irem p banco de dados
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getCliente().getId());
+            stmt.setString(2, obj.getDataVendas());
+            stmt.setDouble(3, obj.getTotalVenda());
+            stmt.setString(4, obj.getObs());
+
+            stmt.execute();
+            stmt.close();
+
+            JOptionPane.showMessageDialog(null, "Venda com Sucesso");
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao reistrar a venda." + e);
+        }
+    }    
+}
