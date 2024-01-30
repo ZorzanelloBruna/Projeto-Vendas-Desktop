82bd833dfe3981092837a3d0172977ef20d7b740
Bruna Zorzanello <brunazorzanello@gmail.com>
2024-01-30 13:57:29 -0300

    abrindo a tela de funcionarios a partir do menu

--- src/projetovendas/view/FrmMenu.java
+++ src/projetovendas/view/FrmMenu.java
@@ -222,7 +222,9 @@ public class FrmMenu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
+        // funcionarios
+        FrmFuncionarios telaFuncioanarios = new FrmFuncionarios();
+        telaFuncioanarios.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
